1b778bfc3393406f4b971b6dc7f4f1d1b16c804e
Leng <no@gmail.com>
2024-05-20 09:39:02 +0800

    Initial commit

---
+++ src/main/java/com/example/springsecuritydemo/controller/RedisController.java
@@ -0,0 +1,42 @@
+package com.example.springsecuritydemo.controller;
+
+import com.example.springsecuritydemo.util.RedisCache;
+import com.example.springsecuritydemo.util.TokenUtil;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/test")
+public class RedisController {
+
+    private final RedisCache redisCache;
+
+    public RedisController(RedisCache redisCache) {
+        this.redisCache = redisCache;
+    }
+
+    @RequestMapping("/redis")
+    public String getRedis() {
+        redisCache.setCacheObject("test", "test");
+        return redisCache.getCacheObject("test").toString();
+    }
+
+    @RequestMapping("/user")
+    public String getUser() {
+        String qqq = "134 ".trim();
+        return "登入成功拿到User";
+    }
+
+    @RequestMapping("/jjwt")
+    public Map<String, String> jjwt() {
+        Map<String, String> map = new HashMap<>();
+        String token = TokenUtil.genAccessToken("Leo");
+        map.put("encoding", token);
+        return map;
+
+    }
+
+}
